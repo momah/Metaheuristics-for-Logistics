4b536b143cc70a123cdd34d4a578cee94998a376
Laura Inés González González <72380046+alu0101332819@users.noreply.github.com>
2023-12-06 21:33:34 +0000

    Update BPPGreedyTest.java

--- src/test/java/concrete/metaheuristics/containerBPP/BPPGreedyTest.java
+++ src/test/java/concrete/metaheuristics/containerBPP/BPPGreedyTest.java
@@ -1,9 +1,5 @@
 package main.java.concrete.metaheuristics.containerBPP;
 
-import main.java.abstracts.problem.ILoadingProblems;
-import org.junit.jupiter.api.*;
-import static org.junit.jupiter.api.Assertions.*;
-
 public class BPPGreedyTest {
 
 }
