4b488d026da549b2ec5aba6006e359116d07d5de
Laura Inés González González <72380046+alu0101332819@users.noreply.github.com>
2023-12-06 17:51:37 +0000

    Update MainTest.java

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,5 +1,28 @@
-package test.java.menus;
+package test.java;
 
-class MainTest {
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class MainTest {
+
+    @Test
+    public void testMainWithArguments() {
+        String[] args = {"dataFile.txt"};
+        assertDoesNotThrow(() -> Main.main(args));
+    }
+
+    @Test
+    public void testMainWithoutArguments() {
+        InputStream sysInBackup = System.in;
+        ByteArrayInputStream in = new ByteArrayInputStream("0\n".getBytes());
+        System.setIn(in);
+
+        assertDoesNotThrow(() -> Main.main(new String[0]));
+
+        System.setIn(sysInBackup);
+    }
 }
