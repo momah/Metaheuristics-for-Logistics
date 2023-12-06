fc12bfcb48e28288cd2f710073690e33365da190
Laura Inés González González <72380046+alu0101332819@users.noreply.github.com>
2023-12-06 22:27:34 +0000

    Update Solution.java

--- src/main/java/abstracts/solution/Solution.java
+++ src/main/java/abstracts/solution/Solution.java
@@ -7,68 +7,68 @@ import main.java.abstracts.problem.Problem;
  * @author mmahrach
  *
  */
-public abstract class Solution implements Cloneable,Comparable<Object> {
-	/**
-	 * The problem represents the Problem class object, which is an instance of the input data to solve.
-	 */
-	protected Problem problem;
-	
-	/**
-	 * The outlput solution dataset of the problem.
-	 */
-	protected int[] solution;
-	
-	/**
-	 * Metaheuristics used for resolution
-	 */
-	protected RoutingAbsractMHeuristics metaheuristics;
-	
-	/**
-	 * 
-	 */
-	public Solution() {
-		super();
-	}
+public abstract class Solution implements Cloneable, Comparable<Object> {
+    /**
+     * The problem represents the Problem class object, which is an instance of the input data to solve.
+     */
+    protected Problem problem;
 
-	/**
-	 * @return the solution
-	 */
-	public int[] getSolution() {
-		return solution;
-	}
+    /**
+     * The output solution dataset of the problem.
+     */
+    protected int[] result;  // Renamed from "solution"
 
-	/**
-	 * @param solution the solution to set
-	 */
-	public void setSolution(int[] solution) {
-		this.solution = solution;
-	}
+    /**
+     * Metaheuristics used for resolution
+     */
+    protected RoutingAbsractMHeuristics metaheuristics;
 
-	/**
-	 * @return the problem
-	 */
-	public Problem getProblem() {
-		return problem;
-	}
+    /**
+     *
+     */
+    protected Solution() {
+        super();
+    }
 
-	/**
-	 * @param problem the problem to set
-	 */
-	public void setProblem(Problem problem) {
-		this.problem = problem;
-	}
-	
-	/**
-	 * @return the metaheuristics of solution
-	 */
-	public RoutingAbsractMHeuristics getMetaheuristics() {
-		return metaheuristics;
-	}
+    /**
+     * @return the result
+     */
+    public int[] getResult() {  // Renamed from "getSolution"
+        return result;
+    }
 
-	/**
-	 * @param metaheuristics, the metaheuristics to set to the solution
-	 */
-	public void setMetaheuristics(RoutingAbsractMHeuristics metaheuristics) {
-		this.metaheuristics = metaheuristics;
-	}
+    /**
+     * @param result the result to set
+     */
+    public void setResult(int[] result) {  // Renamed from "setSolution"
+        this.result = result;
+    }
+
+    /**
+     * @return the problem
+     */
+    public Problem getProblem() {
+        return problem;
+    }
+
+    /**
+     * @param problem the problem to set
+     */
+    public void setProblem(Problem problem) {
+        this.problem = problem;
+    }
+
+    /**
+     * @return the metaheuristics of solution
+     */
+    public RoutingAbsractMHeuristics getMetaheuristics() {
+        return metaheuristics;
+    }
+
+    /**
+     * @param metaheuristics, the metaheuristics to set to the solution
+     */
+    public void setMetaheuristics(RoutingAbsractMHeuristics metaheuristics) {
+        this.metaheuristics = metaheuristics;
+    }
 }
