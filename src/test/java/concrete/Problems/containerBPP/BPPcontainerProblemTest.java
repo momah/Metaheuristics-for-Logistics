695a852b0b352f9a30e9f666dcd2bd49b293ed25
Eduardo José Barrios García <eduardojbarriosgarcia@gmail.com>
2023-12-06 21:20:22 +0000

    Adding BPPcontainerTest

--- src/test/java/concrete/Problems/containerBPP/BPPcontainerProblemTest.java
+++ src/test/java/concrete/Problems/containerBPP/BPPcontainerProblemTest.java
@@ -1,5 +1,115 @@
-package test.java.concrete.Problems.containerBPP;
+package main.java.concrete.Problems.containerBPP;
 
-class BPPcontainerProblemTest {
+import java.util.ArrayList;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
+public class BPPcontainerProblemTest {
+	@Test
+	public void BPPcontainerProblem() {
+		int capacity = 123;
+		ArrayList<Item> items = null;
+		BPPcontainerProblem expected = new BPPcontainerProblem("abc");
+		BPPcontainerProblem actual = new BPPcontainerProblem(capacity, items);
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void BPPcontainerProblemTODO() {
+		BPPcontainerProblem expected = new BPPcontainerProblem("abc");
+		BPPcontainerProblem actual = new BPPcontainerProblem();
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void BPPcontainerProblemTODOTODO() {
+		String filename = "abc";
+		BPPcontainerProblem expected = new BPPcontainerProblem("abc");
+		BPPcontainerProblem actual = new BPPcontainerProblem(filename);
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void LoadDataFromFile() {
+		BPPcontainerProblem b = new BPPcontainerProblem("abc");
+		String filename = "abc";
+		String expected = "abc";
+		String actual = b.LoadDataFromFile(filename);
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void getCapacity() {
+		BPPcontainerProblem b = new BPPcontainerProblem("abc");
+		int expected = 123;
+		int actual = b.getCapacity();
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void getItem() {
+		BPPcontainerProblem b = new BPPcontainerProblem("abc");
+		int index = 123;
+		Item expected = new Item(123, 123);
+		Item actual = b.getItem(index);
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void getItems() {
+		BPPcontainerProblem b = new BPPcontainerProblem("abc");
+		ArrayList<Item> expected = null;
+		ArrayList<Item> actual = b.getItems();
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void getNumItems() {
+		BPPcontainerProblem b = new BPPcontainerProblem("abc");
+		int expected = 123;
+		int actual = b.getNumItems();
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void instancesFromText() {
+		BPPcontainerProblem b = new BPPcontainerProblem("abc");
+		b.instancesFromText();
+	}
+
+	@Test
+	public void pesoItems() {
+		BPPcontainerProblem b = new BPPcontainerProblem("abc");
+		double expected = 123.4D;
+		double actual = b.pesoItems();
+
+		assertEquals(expected, actual, 0.0000001D);
+	}
+
+	@Test
+	public void toStringTODO() {
+		BPPcontainerProblem b = new BPPcontainerProblem("abc");
+		boolean outputitems = true;
+		String expected = "abc";
+		String actual = b.toString(outputitems);
+
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void toStringTODOTODO() {
+		BPPcontainerProblem b = new BPPcontainerProblem("abc");
+		String expected = "abc";
+		String actual = b.toString();
+
+		assertEquals(expected, actual);
+	}
 }
