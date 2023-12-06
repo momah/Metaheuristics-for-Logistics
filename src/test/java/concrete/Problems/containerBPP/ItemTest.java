ff97d5c63d9d40ad14dc85b045e11605d6568787
Laura Inés González González <72380046+alu0101332819@users.noreply.github.com>
2023-12-06 19:20:54 +0000

    Update ItemTest.java

--- src/test/java/concrete/Problems/containerBPP/ItemTest.java
+++ src/test/java/concrete/Problems/containerBPP/ItemTest.java
@@ -1,52 +1,55 @@
 package main.java.concrete.Problems.containerBPP;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ItemTest {
-	@Test
-	public void Item() {
-		int itemArea = 123;
-		int priority = 123;
-		Item expected = new Item(123, 123);
-		Item actual = new Item(itemArea, priority);
 
-		assertEquals(expected, actual);
-	}
+    @Test
+    public void testItemConstructor() {
+        int itemArea = 123;
+        int priority = 123;
+        Item expected = new Item(123, 123);
+        Item actual = new Item(itemArea, priority);
 
-	@Test
-	public void getFator() {
-		Item i = new Item(123, 123);
-		double expected = 1.0D;
-		double actual = i.getFator();
+        assertEquals(expected.getWeight(), actual.getWeight());
+        assertEquals(expected.getItemPriority(), actual.getItemPriority());
+        assertEquals(expected.toString(), actual.toString());
+    }
 
-		assertEquals(expected, actual, 0.0000001D);
-	}
+    @Test
+    public void testGetFator() {
+        Item i = new Item(123, 123);
+        double expected = 1.0D;
+        double actual = i.getFator();
 
-	@Test
-	public void getItemPriority() {
-		Item i = new Item(123, 123);
-		int expected = 123;
-		int actual = i.getItemPriority();
+        assertEquals(expected, actual, 0.0000001D);
+    }
 
-		assertEquals(expected, actual);
-	}
+    @Test
+    public void testGetItemPriority() {
+        Item i = new Item(123, 123);
+        int expected = 123;
+        int actual = i.getItemPriority();
 
-	@Test
-	public void getWeight() {
-		Item i = new Item(123, 123);
-		int expected = 123;
-		int actual = i.getWeight();
+        assertEquals(expected, actual);
+    }
 
-		assertEquals(expected, actual);
-	}
+    @Test
+    public void testGetWeight() {
+        Item i = new Item(123, 123);
+        int expected = 123;
+        int actual = i.getWeight();
 
-	@Test
-	public void toStringTODO() {
-		Item i = new Item(123, 123);
-		String expected = "[ Item Area = 123, Priority = 123 ]";
-		String actual = i.toString();
+        assertEquals(expected, actual);
+    }
 
-		assertEquals(expected, actual);
-	}
+    @Test
+    public void testToString() {
+        Item i = new Item(123, 123);
+        String expected = "[ Item Area = 123, Priority = 123 ]";
+        String actual = i.toString();
+
+        assertEquals(expected, actual);
+    }
 }
