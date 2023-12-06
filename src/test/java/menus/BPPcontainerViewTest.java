54a228f129366ff272c3a5d53485f7f190affb25
Laura Inés González González <72380046+alu0101332819@users.noreply.github.com>
2023-12-06 21:27:14 +0000

    Update BPPcontainerViewTest.java

--- src/test/java/menus/BPPcontainerViewTest.java
+++ src/test/java/menus/BPPcontainerViewTest.java
@@ -1,18 +1,5 @@
 package test.java.menus;
 
-import main.java.menus.BPPcontainerView;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.AfterEach;
-import org.mockito.Mockito;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.util.Scanner;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
 public class BPPcontainerViewTest {
 
 }
