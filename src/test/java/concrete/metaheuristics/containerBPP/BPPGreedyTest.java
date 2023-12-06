a118cb8889c8f7be2d52ce6624ce35b08b354e07
Laura Inés González González <72380046+alu0101332819@users.noreply.github.com>
2023-12-06 19:46:23 +0000

    Update BPPGreedyTest.java

--- src/test/java/concrete/metaheuristics/containerBPP/BPPGreedyTest.java
+++ src/test/java/concrete/metaheuristics/containerBPP/BPPGreedyTest.java
@@ -5,15 +5,5 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BPPGreedyTest {
-	@Test
-	public void printBestSolution() {
-		BPPGreedy b = new BPPGreedy(null);
-		b.printBestSolution();
-	}
 
-	@Test
-	public void solve() {
-		BPPGreedy b = new BPPGreedy(null);
-		b.solve();
-	}
 }
