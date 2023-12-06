2077106b7ab06437fb33ee9bac58e67de407da57
Laura Inés González González <72380046+alu0101332819@users.noreply.github.com>
2023-12-06 19:26:53 +0000

    Update BPPcontainerViewTest.java

--- src/test/java/menus/BPPcontainerViewTest.java
+++ src/test/java/menus/BPPcontainerViewTest.java
@@ -15,44 +15,4 @@ import static org.mockito.Mockito.*;
 
 public class BPPcontainerViewTest {
 
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private final PrintStream originalOut = System.out;
-    private Scanner scannerMock;
-
-    @BeforeEach
-    void setUp() {
-        System.setOut(new PrintStream(outContent));
-        scannerMock = Mockito.mock(Scanner.class);
-    }
-
-    @AfterEach
-    void tearDown() {
-        System.setOut(originalOut);
-    }
-
-    @Test
-    void testRun_withValidDataFile() {
-        try {
-            String dataFile = "valid_data.txt";
-            BPPcontainerView.run(scannerMock, dataFile);
-            assertTrue(outContent.toString().contains("BPP Container Loading Problem"));
-        } catch (CloneNotSupportedException e) {
-            fail("CloneNotSupportedException should not have been thrown");
-        }
-    }
-
-    @Test
-    void testRun_exitCommand() {
-        try {
-            String input = "exit";
-            when(scannerMock.nextLine()).thenReturn(input);
-            BPPcontainerView.run(scannerMock, null);
-            assertTrue(outContent.toString().contains("exit"));
-        } catch (CloneNotSupportedException e) {
-            fail("CloneNotSupportedException should not have been thrown");
-        }
-    }
-
-    // Additional tests...
-
 }
