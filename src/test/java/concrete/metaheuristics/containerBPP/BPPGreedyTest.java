e8f7b637fe54f2900ee58914cbc39124b943e79f
Laura Inés González González <72380046+alu0101332819@users.noreply.github.com>
2023-12-06 19:43:35 +0000

    Update BPPGreedyTest.java

--- src/test/java/concrete/metaheuristics/containerBPP/BPPGreedyTest.java
+++ src/test/java/concrete/metaheuristics/containerBPP/BPPGreedyTest.java
@@ -1,43 +1,28 @@
 package main.java.concrete.metaheuristics.containerBPP;
 
 import main.java.abstracts.problem.ILoadingProblems;
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BPPGreedyTest {
-    @Test
-    public void testBPPGreedyConstructor() {
-        ILoadingProblems instancia = null;
-        BPPGreedy expected = new BPPGreedy(null);
-        BPPGreedy actual = new BPPGreedy(instancia);
+	@Test
+	public void BPPGreedy() {
+		ILoadingProblems instancia = null;
+		BPPGreedy expected = new BPPGreedy(null);
+		BPPGreedy actual = new BPPGreedy(instancia);
 
-        assertEquals(expected, actual);
-    }
+		assertEquals(expected, actual);
+	}
 
-    @Test
-    public void testPrintBestSolution() {
-        BPPGreedy b = new BPPGreedy(null);
+	@Test
+	public void printBestSolution() {
+		BPPGreedy b = new BPPGreedy(null);
+		b.printBestSolution();
+	}
 
-        // Assuming printBestSolution returns a string, you might want to check for a non-null result
-        assertNotNull(b.printBestSolution());
-    }
-
-    @Test
-    public void testSolve() {
-        ILoadingProblems loadingProblem = null;
-        BPPGreedy b = new BPPGreedy(loadingProblem);
-
-        // Assuming solve method updates some state, you might want to check for its side effects
-        try {
-            b.solve();
-            // Add any additional assertions or checks related to the side effects of solve method
-            // For example, if there are some properties that should be updated after solve, you can check them here.
-            // assertEquals(expectedValue, b.getSomeProperty());
-        } catch (Exception e) {
-            // Modify the failure message as needed
-            throw new AssertionError("Unexpected exception: " + e.getMessage(), e);
-        }
-    }
+	@Test
+	public void solve() {
+		BPPGreedy b = new BPPGreedy(null);
+		b.solve();
+	}
 }
