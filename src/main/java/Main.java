1c9329af6b431de3b63a51cf1c2017491b5a1b83
Laura Inés González González <72380046+alu0101332819@users.noreply.github.com>
2023-12-06 21:17:53 +0000

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,17 +4,20 @@ import main.java.menus.BPPcontainerView;
 import main.java.menus.VRPvehiclesView;
 
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class Main {
-    static final int OPCION1 = 1;
-    static final int OPCION2 = 2;
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+    private static final String SEPARATOR_LINE = "------------------------------------------------";
+    private static final int OPCION1 = 1;
+    private static final int OPCION2 = 2;
     private static String dataFile = null;
 
     public static void main(String[] args) {
         // Verificar si se han proporcionado argumentos de línea de comandos
         if (args.length > 0) {
             dataFile = args[0];
-            System.out.println("Archivo de datos seleccionado: " + dataFile);
+            logger.info("Archivo de datos seleccionado: " + dataFile);
         }
 
         Scanner sc = new Scanner(System.in);
@@ -31,7 +34,7 @@ public class Main {
                     VRPvehiclesView.run(sc, dataFile);
                 }
             } catch (CloneNotSupportedException e) {
-                e.printStackTrace();
+                logger.severe("Error durante la ejecución: " + e.getMessage());
             }
 
         } while (opcion != 0);
@@ -39,22 +42,21 @@ public class Main {
         sc.close(); // Cerrar el Scanner al final del programa
     }
 
-
-	public static void printMenu() {
-		String[] menu = {
-				"------------------------------------------------",
-				"---------------- MENÚ PRINCIPAL ----------------",
-				"------------------------------------------------",
-				" Elige una metaheurística: ----------------------",
-				"------------------------------------------------",
-				"1. BPP Container Loading Problem                -",
-				"2. VRP Vehicle Routing Problem                  -",
-				"0. Salir                                        -",
-				"------------------------------------------------",
-		};
-		for (String line : menu) {
-			System.out.println(line);
-		}
-		System.out.print("Opción: ");
-	}
+    private static void printMenu() {
+        String[] menu = {
+                SEPARATOR_LINE,
+                "---------------- MENÚ PRINCIPAL ----------------",
+                SEPARATOR_LINE,
+                " Elige una metaheurística: ----------------------",
+                SEPARATOR_LINE,
+                "1. BPP Container Loading Problem                -",
+                "2. VRP Vehicle Routing Problem                  -",
+                "0. Salir                                        -",
+                SEPARATOR_LINE,
+        };
+        for (String line : menu) {
+            logger.info(line);
+        }
+        logger.info("Opción: ");
+    }
 }
