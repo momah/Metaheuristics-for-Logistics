3568641850f78f93ce483d19a9b6e79914a39696
Laura Inés González González <72380046+alu0101332819@users.noreply.github.com>
2023-12-06 19:44:35 +0000

    Update BPPGreedyTest.java

--- src/test/java/concrete/metaheuristics/containerBPP/BPPGreedyTest.java
+++ src/test/java/concrete/metaheuristics/containerBPP/BPPGreedyTest.java
@@ -6,15 +6,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class BPPGreedyTest {
 	@Test
-	public void BPPGreedy() {
-		ILoadingProblems instancia = null;
-		BPPGreedy expected = new BPPGreedy(null);
-		BPPGreedy actual = new BPPGreedy(instancia);
-
-		assertEquals(expected, actual);
-	}
-
-	@Test
 	public void printBestSolution() {
 		BPPGreedy b = new BPPGreedy(null);
 		b.printBestSolution();
